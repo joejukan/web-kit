04c9f9f92faf47851b5ae7a5e86285075c6f003a
joejukan <jeniojukan@gmail.com>
2022-01-30 03:39:49 -0500

    [CLEANUP] Remove Console Log from Clone Unit Tests

--- spec/reflection/clone.spec.ts
+++ spec/reflection/clone.spec.ts
@@ -5,7 +5,6 @@ describe('clone unit tests', () => {
   test('clone an instance of a basic class', () => {
     const basic = new Basic();
     const result = clone(basic);
-    console.info({ basic, result });
     expect(result).toBeInstanceOf(Basic);
   });
 });
